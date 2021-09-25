c3fb234bd1d315cf565a687461dc55af38aba6fc
Aleksandr Kulyk <ale.kulyk@gmail.com>
2021-09-26 01:18:12 +0300

    Завершил ДЗ 6

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formInput = document.querySelector(".login-form");
 
+formInput.addEventListener('submit', inputSubmit);
+
+function inputSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+console.log(event.currentTarget);
+
+  if (email.value === "" || password.value === "") {
+    
+    return alert('Please fill in all the fields!');
+  } 
+
+  const users = {
+      email: email.value,
+      password: password.value,
+  }
+  console.log(users);
+  e.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputColor = document.querySelector('.change-color');
+console.log(inputColor);
+const spanText = document.querySelector('.color');
+inputColor.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  spanText.textContent = color;
+  document.body.style.background = color;
+});
+
+
+
